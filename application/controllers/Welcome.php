4e092b61d06e3fcc83b21f031f1faa4d5ef5a8f1
ilhamdika <ilham27dika@gmail.com>
2025-02-01 11:35:42 +0700

    feat: send data category into view

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,17 +6,19 @@ class Welcome extends MY_Controller
 
 	public function bisa_dijual()
 	{
-		$status = $this->db->select('*')->from('status')->get()->result();
 		$kategori = $this->db->select('*')->from('kategori')->get()->result();
 
 		$this->load_view('produk/bisa_dijual', [
-			'status' => $status,
 			'kategori' => $kategori
 		]);
 	}
 
 	public function tidak_bisa_dijual()
 	{
-		$this->load_view('produk/tidak_bisa_dijual');
+		$kategori = $this->db->select('*')->from('kategori')->get()->result();
+
+		$this->load_view('produk/tidak_bisa_dijual', [
+			'kategori' => $kategori
+		]);
 	}
 }
