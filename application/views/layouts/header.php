e2db01dc1bb56798d04e0cc0171823916a695cb7
ilhamdika <ilham27dika@gmail.com>
2025-02-01 11:28:27 +0700

    feat: change header

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -6,25 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= isset($title) ? $title : 'My Website'; ?></title>
 
-    <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
 
-    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="<?= base_url(); ?>">CodeIgniter 3</a>
+            <a class="navbar-brand" href="<?= base_url(); ?>">Test</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <!-- <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url(); ?>">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url('about'); ?>">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= base_url('contact'); ?>">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= base_url(); ?>">Produk</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= base_url('about'); ?>">Tidak bisa dijual</a></li>
                 </ul>
-            </div>
+            </div> -->
         </div>
     </nav>
