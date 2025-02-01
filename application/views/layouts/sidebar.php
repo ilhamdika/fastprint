941e010b2811d605592a2988566902c117aaf3a1
ilhamdika <ilham27dika@gmail.com>
2025-02-01 11:26:45 +0700

    feat: active sidebar

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -1,5 +1,13 @@
-<div class="list-group">
-    <a href="<?= base_url(); ?>" class="list-group-item list-group-item-action">Dashboard</a>
-    <a href="<?= base_url('posts'); ?>" class="list-group-item list-group-item-action">Posts</a>
-    <a href="<?= base_url('users'); ?>" class="list-group-item list-group-item-action">Users</a>
+<div class="sidebar">
+    <div class="list-group">
+        <a href="<?= base_url(); ?>"
+            class="list-group-item list-group-item-action custom-list-item <?= $this->uri->segment(1) == '' ? 'active' : ''; ?>">
+            <i class="fas fa-box"></i> Produk
+        </a>
+
+        <a href="<?= base_url('welcome/tidak_bisa_dijual'); ?>"
+            class="list-group-item list-group-item-action custom-list-item <?= $this->uri->segment(2) == 'tidak_bisa_dijual' ? 'active' : ''; ?>">
+            <i class="fas fa-ban"></i> Tidak Bisa Dijual
+        </a>
+    </div>
 </div>
