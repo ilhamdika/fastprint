314cc666620be65dd9fabaa713c82de88d79710f
ilhamdika <ilham27dika@gmail.com>
2025-02-01 11:29:26 +0700

    feat: modify controller welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,8 +4,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Welcome extends MY_Controller
 {
 
-	public function index()
+	public function bisa_dijual()
 	{
-		$this->load_view('welcome_message');
+		$status = $this->db->select('*')->from('status')->get()->result();
+		$kategori = $this->db->select('*')->from('kategori')->get()->result();
+
+		$this->load_view('produk/bisa_dijual', [
+			'status' => $status,
+			'kategori' => $kategori
+		]);
+	}
+
+	public function tidak_bisa_dijual()
+	{
+		$this->load_view('produk/tidak_bisa_dijual');
 	}
 }
