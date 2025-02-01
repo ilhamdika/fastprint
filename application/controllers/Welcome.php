8b0577d2f203e0aff40063f388c55b468c87b114
ilhamdika <ilham27dika@gmail.com>
2025-02-01 12:36:48 +0700

    fix: none controller welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,21 +4,4 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Welcome extends MY_Controller
 {
 
-	public function bisa_dijual()
-	{
-		$kategori = $this->db->select('*')->from('kategori')->get()->result();
-
-		$this->load_view('produk/bisa_dijual', [
-			'kategori' => $kategori
-		]);
-	}
-
-	public function tidak_bisa_dijual()
-	{
-		$kategori = $this->db->select('*')->from('kategori')->get()->result();
-
-		$this->load_view('produk/tidak_bisa_dijual', [
-			'kategori' => $kategori
-		]);
-	}
 }
