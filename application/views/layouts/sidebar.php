492f4b28f91acb5ba47e4b3b4be9f984cc52571e
ilhamdika <ilham27dika@gmail.com>
2025-02-01 12:37:20 +0700

    fix: change redirect sidebar

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -5,9 +5,14 @@
             <i class="fas fa-box"></i> Produk
         </a>
 
-        <a href="<?= base_url('welcome/tidak_bisa_dijual'); ?>"
+        <a href="<?= base_url('data/tidak_bisa_dijual'); ?>"
             class="list-group-item list-group-item-action custom-list-item <?= $this->uri->segment(2) == 'tidak_bisa_dijual' ? 'active' : ''; ?>">
             <i class="fas fa-ban"></i> Tidak Bisa Dijual
+        </a>
+
+        <a href="<?= base_url('data/add_produk'); ?>"
+            class="list-group-item list-group-item-action custom-list-item <?= $this->uri->segment(2) == 'add_produk' ? 'active' : ''; ?>">
+            <i class="fas fa-plus"></i> Tambah Produk
         </a>
     </div>
 </div>
